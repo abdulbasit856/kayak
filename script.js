d22f9ca23c7a40ce446076b57d7c46c2bffad430
abdulbasit856 <abdulbasitejaz856@gmail.com>
2024-11-17 19:30:09 +0500

    kayak project

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("heart").addEventListener("click", function () {
+  this.classList.toggle("clicked");
+});
+document.getElementById("heart1").addEventListener("click", function () {
+  this.classList.toggle("clicked");
+});
+document.getElementById("heart2").addEventListener("click", function () {
+  this.classList.toggle("clicked");
+});
